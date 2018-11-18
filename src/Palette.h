768ffa966be8cc421afd6583589933d0ce5da0a4
Jack McKiernan <JMCKIERN@tcd.ie>
2018-11-18 23:42:00 +0000

    Moved ntscpalette.pal into Palette.cpp
    The functionality to load a .pal file is still
    there.

--- src/Palette.h
+++ src/Palette.h
@@ -5,15 +5,11 @@
 
 class Palette {
 private:
-	uint8_t* palette;
-	bool loadedFromFile = false;
-	void Transfer(uint8_t* arr, int length);
-	void LoadDefault();
+	uint8_t palette[0x600];
 	void LoadPal(std::string palFile);
 
 public:
 	Colour GetColour(uint8_t colourNum, uint8_t colourEmp);
 	Palette(std::string palFile);
 	Palette();
-	~Palette();
 };
