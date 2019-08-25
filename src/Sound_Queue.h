b69636540a792790da51d853f20ff62af329f329
JMcKiern <jmckiern@tcd.ie>
2019-08-25 21:06:51 +0100

    Added Appveyor support (#44)
    * Initial appveyor commit
    
    * Retabbed to spaces
    
    * Added config for *.yml files
    
    * Fixed typo
    
    * Added vcpkg triplet and removed one line of indenting
    
    * Added line to update vcpkg
    
    * Changed to VS2017
    
    * Removed updating of vcpkg
    
    * Fixed broken cmake command
    
    * Added vcpkg packages to cache
    
    * Updated CMakeLists.txt to work with vcpkg
    
    * Added *.cmake so that it would work on linux
    
    * Update appveyor.yml
    
    * message() to stdout not stderr
    
    * Update appveyor.yml
    
    * Removed CONFIG from find_package()
    
    * Different library linking if using vcpkg or not
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update appveyor.yml
    
    * Update appveyor.yml
    
    * Update appveyor.yml
    
    * Update appveyor.yml
    
    * Update appveyor.yml
    
    * Update appveyor.yml
    
    * Update appveyor.yml

--- src/Sound_Queue.h
+++ src/Sound_Queue.h
@@ -6,25 +6,25 @@
 #ifndef SOUND_QUEUE_H
 #define SOUND_QUEUE_H
 
-#include "SDL.h"
+#include <SDL2/SDL.h>
 
 // Simple SDL sound wrapper that has a synchronous interface
 class Sound_Queue {
 public:
 	Sound_Queue();
 	~Sound_Queue();
-	
+
 	// Initialize with specified sample rate and channel count.
 	// Returns NULL on success, otherwise error string.
 	const char* init( long sample_rate, int chan_count = 1 );
-	
+
 	// Number of samples in buffer waiting to be played
 	int sample_count() const;
-	
+
 	// Write samples to buffer and block until enough space is available
 	typedef short sample_t;
 	void write( const sample_t*, int count );
-	
+
 private:
 	enum { buf_size = 2048 };
 	enum { buf_count = 3 };
@@ -34,7 +34,7 @@ private:
 	int write_buf;
 	int write_pos;
 	bool sound_open;
-	
+
 	sample_t* buf( int index );
 	void fill_buffer( Uint8*, int );
 	static void fill_buffer_( void*, Uint8*, int );
